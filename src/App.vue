2f30f0455da860e14397382cc81f333805bf926c
felkira <fajarmaulana_math@students.unnes.ac.id>
2022-03-17 16:25:48 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <section class="app">
         <h1>Create your task</h1>
         <div class="task-input">
-            <input type="text" spellcheck="false" placeholder="Input your task here ..." v-model="tasks.name" @keyup.enter="newTask">
+            <input type="text" spellcheck="false" placeholder="Input your task here ..." v-model="task" @keyup.enter="newTask">
             <button title="Add Task" @click="newTask"><i class="fa-solid fa-plus"></i></button>
         </div>
         <ul>
             <li class="task-item" v-for="task in tasks" :key="task.id">
                 <input type="checkbox">
-                <span>{{ task.name }}</span>
-                <i class="fa-solid fa-trash-can" title="Delete Task" @click="delTask(task)">{{ task.del }}</i>
+                <span>{{ task }}</span>
+                <i class="fa-solid fa-trash-can" title="Delete Task" @click="delTask(item)"></i>
             </li>
         </ul>
     </section>
@@ -19,24 +19,19 @@
 export default {
   data() {
         return {
+            task: '',
             tasks: [
-                {name: 'Here is your task'},
+                'Here is your task',
             ],
         }
   },
   methods: {
       newTask() {
-          if (!this.tasks.name) {
-              return
-          }
-          this.tasks.push({
-              name: this.tasks.name,
-              del: '',
-          });
-          this.tasks.name = '';
+          this.tasks.push(this.task);
+          this.task = ''
       },
-      delTask(task) {
-          this.tasks.splice(this.tasks.indexOf(task), 1);
+      delTask(item) {
+          this.tasks.splice(item, 1);
       }
   },
 }
